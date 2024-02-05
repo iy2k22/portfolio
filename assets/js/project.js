04eaa1141112f8a6131e91753fd792326eec3be8
Ibrahim Yusuf <iy830755@gmail.com>
2024-02-05 20:12:03 +0000

    add descriptions

--- assets/js/project.js
+++ assets/js/project.js
@@ -3,43 +3,49 @@ const projData = {
         name: "NC Games (Backend)",
         repo: "https://github.com/iy2k22/be-nc-games",
         lang: ["JavaScript"],
-        img: "../assets/images/nc-games-be.png"
+        img: "../assets/images/nc-games-be.png",
+        desc: "The first part of my NC Games app. This is an API that leverages Express and NodePostgres to access data stored in an SQL database."
     },
     "fe-nc-games": {
         name: "NC Games (Frontend)",
         repo: "https://github.com/iy2k22/nc-games-frontend",
         lang: ["React"],
-        img: "../assets/images/nc-games-fe.png"
+        img: "../assets/images/nc-games-fe.png",
+        desc: "The second of my NC Games app. This utilises Axios to fetch data and React to put it on the screen."
     },
     "benchit": {
         name: "benchIt",
         repo: "https://github.com/valentinbiz/benchIt",
         lang: ["JavaScript", "React", "React Native"],
-        img: "https://placehold.co/600x400"
+        img: "https://placehold.co/600x400",
+        desc: "The final project of the Northcoders bootcamp. This is an Android app that uses React Native."
     },
     "garden": {
         name: "Gardener's Little Helper",
         repo: "https://github.com/Oxanal3891/gardening-app",
         lang: ["HTML", "CSS", "Bootstrap", "JavaScript", "jQuery"],
-        img: "../assets/images/garden.png"
+        img: "../assets/images/garden.png",
+        desc: "The first project of the edX front-end bootcamp. It's not the most advanced project but this was where I really got to hone my jQuery skills."
     },
     "error": {
         name: "Error",
         repo: "./projects.html" ,
         lang: [],
-        img: "https://placehold.co/600x400"
+        img: "https://placehold.co/600x400",
+        desc: "It appears you've tried to access a project that doesn't exist. Perhaps you've made a typo?"
     }
 };
 
 $(document).ready(() => {
     const search = new URLSearchParams(window.location.search);
-    const { name, repo, lang, img } = projData[search.get("name")] || projData.error;
+    const { name, repo, lang, img, desc } = projData[search.get("name")] || projData.error;
     $("title").text(name);
     $("#heading h1").text(name);
     
     $("#box-header").text(name);
     $("#project-img").attr("src", img);
     $("#repo-link").attr("href", repo);
+    $("#project-description").text(desc);
     
     // add list of tech
     const langList = $("#lang");
